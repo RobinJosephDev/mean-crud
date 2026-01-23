5e39fe8b32e5617e18419692175185c0f846c8d0
Robin Joseph <robin.joseph@vandalayglobal.com>
2026-01-23 19:13:59 +0530

    app. ts updated

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,10 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeesListComponent } from './employees-list/employees-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeesListComponent, MatToolbarModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    EmployeesListComponent,
+    MatToolbarModule,
+  ],
   template: `
     <mat-toolbar>
       <span>Employees Management System</span>
